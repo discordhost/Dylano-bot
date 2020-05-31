84bd1a431cc7ad11763cd180a4ae4a0de8000741
discordhost <66209061+discordhost@users.noreply.github.com>
2020-05-31 17:20:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const discord = require("discord.js");
 const botConfig = require("./botconfig.json");
 const client = new discord.Client();
 
-client.login(botConfig.token);
-
 
 // ONLINE MESSAGE
 client.on("ready", async () => {

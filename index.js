1e0cc3463291247447e83d0c2dadff99b18995fb
discordhost <66209061+discordhost@users.noreply.github.com>
2020-06-01 09:17:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.on("message", async message =>{
 
     var args = messageArray.slice(1);
 
-    var commands = commands(command.slice(prefix.length));
+    var commands = commands.send(command.slice(prefix.length));
 
     if(commands) commands.run(bot,message, args);
 
